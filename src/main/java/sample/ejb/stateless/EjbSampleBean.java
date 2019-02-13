31fc01daa4d9ad60b738e8bff4f0a847b71fc9bd
Koji Arai <jca02266@gmail.com>
2019-02-13 16:07:29 +0900

    Fix typo

--- src/main/java/sample/ejb/stateless/EjbSampleBean.java
+++ src/main/java/sample/ejb/stateless/EjbSampleBean.java
@@ -5,6 +5,6 @@ import javax.ejb.Stateless;
 @Stateless // (mappedName = "ejb_mapname")
 public class EjbSampleBean implements EjbSample {
   public void execute(String text) {
-    System.out.println("Ejb3SampleBean#exec:" + text);
+    System.out.println("EjbSampleBean#exec:" + text);
   }
 }
